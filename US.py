fd59d185e50c635f3d685bc794239d3c32333384
hhd88888 <674446990@qq.com>
2021-12-12 23:46:01 -0600

    add recovered and deaths

--- US.py
+++ US.py
@@ -10,6 +10,8 @@ dirPath ="./COVID-19/csse_covid_19_data/csse_covid_19_daily_reports_us"
 def get_data(state_name, dirPath):
     confirms = []
     days = []
+    deaths = []
+    recovered = []
     files = os.listdir(dirPath)
     for file in files:
         if not os.path.isdir(dirPath + '/' + file) and file != "README.md":
@@ -17,20 +19,27 @@ def get_data(state_name, dirPath):
             dayData = dayData.fillna(0)
             days.append(datetime.datetime.strptime(dayData.loc[state_name].Last_Update.split(" ")[0], '%Y-%m-%d'))
             confirms.append(dayData.Confirmed.sum())
+            deaths.append(dayData.Deaths.sum())
+            recovered.append(dayData.Deaths.sum())
+
     res = {}
     res["days"] = days
     res["confirms"] = confirms
+    res["deaths"] = deaths
+    res["recovered"] = recovered
     return res
 
 data = get_data("Texas", dirPath)
 
 confirms = data["confirms"]
+deaths = data["deaths"]
+recovered = data["recovered"]
 days = data["days"]
 day_confirm_data = []
 
 # sort array by date
 for i in range(len(confirms)):
-    day_confirm_data.append((days[i], confirms[i]))
+    day_confirm_data.append((days[i], confirms[i], deaths[i], recovered[i]))
 
 def takeFirst(elem):
     return elem[0]
@@ -39,11 +48,19 @@ def takeFirst(elem):
 day_confirm_data.sort(key=takeFirst)
 days = []
 confirms = []
+deaths = []
+recovered = []
 new_confirms = []
+new_death = []
+new_recovered = []
 for i in range(1, len(day_confirm_data)):
     days.append(day_confirm_data[i][0])
     confirms.append(day_confirm_data[i][1])
+    deaths.append(day_confirm_data[i][2])
+    recovered.append(day_confirm_data[i][3])
     new_confirms.append(day_confirm_data[i][1]- day_confirm_data[i-1][1])
+    new_death.append(day_confirm_data[i][2]- day_confirm_data[i-1][2])
+    new_recovered.append(day_confirm_data[i][3]- day_confirm_data[i-1][3])
 
 # plot original picture
 # plt.plot_date(days, new_confirms, "r")
@@ -51,25 +68,27 @@ for i in range(1, len(day_confirm_data)):
 
 
 #fft
-fft_new_confirms = fft.fft(new_confirms)
-fft_new_confirms1 = abs(fft_new_confirms)/(len(new_confirms) / 2)
-fft_new_confirms2 = fft_new_confirms1[range(len(fft_new_confirms1) // 2)]
-freq = fft.fftfreq(len(fft_new_confirms), d = 1)
+def fftFilter(data):
+    fft_new_data = fft.fft(data)
+    fft_new_data1 = abs(fft_new_data)/(len(new_data) / 2)
+    fft_new_data2 = fft_new_data1[range(len(fft_new_data1) // 2)]
+    freq = fft.fftfreq(len(fft_new_data), d = 1)
 
-# threshold for freq
-threshold = 0.05
+    # threshold for freq
+    threshold = 0.05
 
-for i in range(len(freq)):
-    if freq[i] > threshold or freq[i] < -threshold:
-        fft_new_confirms[i] = 0
+    for i in range(len(freq)):
+        if freq[i] > threshold or freq[i] < -threshold:
+            fft_new_data[i] = 0
 
-# ifft get the value
-ifft_new_confirms = fft.ifft(fft_new_confirms).real
-plt.xlabel('Date')
-plt.plot_date(days, new_confirms, "b", label="original")
-plt.plot_date(days, ifft_new_confirms, "r", label="afterFFT")
-plt.legend()
-plt.show()
+    # ifft get the value
+    ifft_new_data = fft.ifft(fft_new_data).real
+    plt.xlabel('Date')
+    plt.plot_date(days, data, "b", label="original")
+    plt.plot_date(days, ifft_new_data, "r", label="afterFFT")
+    plt.legend()
+    plt.show()
+    return ifft_new_data
 
 # plot fft picture
 # plt.plot(freq, fft_new_confirms, "b")
@@ -85,7 +104,12 @@ for i in range(len(days)):
 new_data["date"] = str_date
 new_data["total_confirmed"] = confirms
 new_data["new_confirmed"] = new_confirms
-new_data["new_confirmed_after_fft"] = ifft_new_confirms
+new_data["new_confirmed_after_fft"] = fftFilter(new_confirms)
+new_data["total_deaths"] = deaths
+new_data["new_deaths"] = new_death
+new_data["new_death_after_fft"] = fftFilter(new_death)
+new_data["total_recovered"] = recovered
 
 new_csv = pd.DataFrame(new_data)
+new_csv.to_csv("./mydata/Texas.csv")
 new_csv.to_csv("./mydata/US.csv")
