fee8ba041b5ea6bf2b9c5c1f01fccf797f67fe0e
cc98mengxinghun <hm45@rice.edu>
2021-12-12 20:19:15 -0600

    Update io.py

--- FFT_Function/io.py
+++ FFT_Function/io.py
@@ -5,6 +5,7 @@ from scipy.signal import savgol_filter
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.interpolate import make_interp_spline
+from scipy.fftpack import fft
 
 dirPath ="./COVID-19/csse_covid_19_data/csse_covid_19_daily_reports_us"
 
@@ -32,6 +33,8 @@ def get_data(state_name, dirPath):
 data = get_data("Texas", dirPath)
 
 # asuming the data has two dimmention vector x and y
+x = []
+y = []
 # Savitzky-Golay
 y_smooth = scipy.signal.savgol_filter(y, 53, 3)
 # y_smooth = (y,window_length,k)
@@ -47,5 +50,52 @@ x_smooth = np.linspace(x.min(), x.max(), 300)
 y_smooth = make_interp_spline(x, y)(x_smooth)
 
 # convolve
-np.convolve(x, y, mode=‘full’)  
+
+
+def np_move_avg(a, n, mode="same"):
+    return(np.convolve(a, np.ones((n,))/n, mode=mode))
+
+
+modes = ['full', 'same', 'valid']
+
+for m in modes:
+    plt.plot(np_move_avg(x, y, mode=m))
 # 返回x和y的离散线性卷积, mode:{‘full’, ‘valid’, ‘same’}参数可选，该参数指定np.convolve函数如何处理边缘。
+
+noise_size = 1400
+noise_array = np.random.normal(0, 2, noise_size)
+adc_value = []
+for i in range(noise_size):
+    adc_value.append(0)
+y = np.array(adc_value) + noise_array
+
+yy = fft(y)  # 快速傅里叶变换
+yf = abs(fft(y))        # 取模
+yf1 = abs(fft(y))/((len(y)/2))  # 归一化处理
+yf2 = yf1[range(int(len(y)/2))]  # 由于对称性，只取一半区间
+# 混合波的FFT（双边频率范围）
+xf = np.arange(len(y))
+plt.figure(1)
+plt.plot(xf, yf, 'r')  # 显示原始信号的FFT模值
+plt.title('FFT of Mixed wave(two sides frequency range)',
+          fontsize=7, color='#7A378B')  # 注意这里的颜色可以查询颜色代码表
+
+yy = fft(y)  # 快速傅里叶变换
+yreal = yy.real        # 获取实数部分
+yimag = yy.imag        # 获取虚数部分
+test_y = yy
+for i in range(len(yy)):
+    if i <= 1200 and i >= 200:
+        test_y[i] = 0
+test = fft.ifft(test_y)  # 对变换后的结果应用ifft函数，应该可以近似地还原初始信号。
+y = test
+yy = fft(y)  # 快速傅里叶变换
+yf = abs(fft(y))        # 取模
+yf1 = abs(fft(y))/((len(y)/2))  # 归一化处理
+yf2 = yf1[range(int(len(y)/2))]  # 由于对称性，只取一半区间
+# 混合波的FFT（双边频率范围）
+xf = np.arange(len(y))
+plt.figure(2)
+plt.plot(xf, yf, 'r')  # 显示原始信号的FFT模值
+plt.title('FFT of Mixed wave(two sides frequency range)',
+          fontsize=7, color='#7A378B')  # 注意这里的颜色可以查询颜色代码表
